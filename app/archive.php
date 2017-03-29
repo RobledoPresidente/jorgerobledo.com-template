276471700628da9801d25f129b379453632e80e9
rodcruzh <rodrigo.cruz.hz@gmail.com>
2017-03-28 22:52:34 -0500

    Categoría noticias: estilo y fechas

--- app/archive.php
+++ app/archive.php
@@ -9,6 +9,7 @@
 
 get_header(); ?>
 
+	<div class="container-fluid">
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
@@ -45,6 +46,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+	</div><!-- .container-fluid -->
 
 <?php
 /* get_sidebar(); */

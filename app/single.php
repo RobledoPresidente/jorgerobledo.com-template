b0021e081f88dce096105f323821b61f864639ae
Sebastián Rojas <sebastian@iridian.co>
2017-03-31 03:12:32 -0500

    Ajustes

--- app/single.php
+++ app/single.php
@@ -29,38 +29,38 @@ get_header(); ?>
 
 		</main--><!-- #main -->
 	<!--/div--><!-- #primary -->
+	    
+<div class="container" id="main">
+	<div class="section">
 		
 	<?php
 	while ( have_posts() ) : the_post(); ?>
-	    
-	    <div class="container" id="main">
-	        <div class="section">
-				<header class="row mb-3">
-					<div class="col-md-5 hidden-lg-up">
-						<img class="img-fluid img-thumbnail" src="<?php the_post_thumbnail_url( 'medium_large' ) ?>" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-5 text-right hidden-md-down">
-						<img class="img-fluid img-thumbnail" src="<?php the_post_thumbnail_url( 'medium_large' ) ?>" alt="Generic placeholder image">
-					</div>
-					<div class="col-md-7">
-						<h2 class="mt-0 display-4" id="page-title-origin"><?php the_title(); ?></h2>
-						<blockquote class="blockquote">
-							<p class="mb-0"><?php echo get_the_excerpt(); ?></p>
-						</blockquote>
-						<p class="text-muted"><?php echo 'Hace ' . human_time_diff( get_the_time('U'), current_time('timestamp') ); ?></p>
-						<?php if ( 'post' == get_post_type() && current_user_can('edit_others_posts') ) : ?>
-						<div class="entry-meta">
-							<a href="<?php echo get_edit_post_link(); ?>" class="btn btn-sm btn-primary"><i class="fa fa-pencil" aria-hidden="true"></i> Editar</a>
-						</div><!-- .entry-meta -->
-						<?php endif; ?>
-					</div>
-				</div>
-	            <?php the_content(); ?>
-	        </header>
-	    </div>
+		<header class="row mb-3">
+			<div class="col-md-5 hidden-lg-up">
+				<img class="img-fluid img-thumbnail" src="<?php the_post_thumbnail_url( 'medium_large' ) ?>" alt="Generic placeholder image">
+			</div>
+			<div class="col-md-5 text-right hidden-md-down">
+				<img class="img-fluid img-thumbnail" src="<?php the_post_thumbnail_url( 'medium_large' ) ?>" alt="Generic placeholder image">
+			</div>
+			<div class="col-md-7">
+				<h2 class="mt-0 display-4" id="page-title-origin"><?php the_title(); ?></h2>
+				<blockquote class="blockquote">
+					<p class="mb-0"><?php echo get_the_excerpt(); ?></p>
+				</blockquote>
+				<p class="text-muted"><?php echo 'Hace ' . human_time_diff( get_the_time('U'), current_time('timestamp') ); ?></p>
+				<?php if ( 'post' == get_post_type() && current_user_can('edit_others_posts') ) : ?>
+				<div class="entry-meta">
+					<a href="<?php echo get_edit_post_link(); ?>" class="btn btn-sm btn-primary"><i class="fa fa-pencil" aria-hidden="true"></i> Editar</a>
+				</div><!-- .entry-meta -->
+				<?php endif; ?>
+			</div>
+		</header>
+		<?php the_content(); ?>
 
 	<?php endwhile; // End of the loop. 
 	?>
+	</div>
+</div>
 
 
 <?php
--- app/template-parts/biblioteca.php
+++ app/template-parts/biblioteca.php
@@ -4,33 +4,36 @@ Template Name: Robledo Biblioteca
 */
 get_header(); ?>
 
-<h1>Biblioteca de Jorge Enrique Robledo</h1>
- <div class="section news">
-    <h2><span>noticias</span></h2>
-    <!--TODO: Acá se deben mostrar las 6 noticias (categoría) más recientes, debe ir además, antes de las noticias, espacio para el widget de suscribirse-->
-    <div class="row">      
-    <?php $query = new WP_Query (array(
-        'category_name' => 'noticias-home',
-        'orderby' => 'modified',
-        'order' => 'DESC',
-        'posts_per_page' => 6
-    )); ?>
-    <?php $i = 0; ?>
-    <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
-    <div class="col-md-4">
-        <a href="<?php the_permalink(); ?>" class="card <?php echo ($i == 0 ? 'bg-primary' : ($i == 2 ? 'card-inverse bg-secondary' : ($i == 4 ? 'card-inverse bg-tertiary' : 'bg-calm'))) ?>">
-        <div class="card-image-header" style="background-image: url(<?php the_post_thumbnail_url( 'medium' ) ?>)">
-            <div class="card-img-overlay">
-            <div class="card-content bottom">
-                <h4 class="card-title"><?php the_title() ?></h4>
-                <p class="card-text"><small class="text-muted"><?php echo 'Hace ' . human_time_diff( get_the_time('U'), current_time('timestamp') ); ?></small></p>
+<div class="container" id="main">
+    <h1 class="display-4" id="page-title-origin">Biblioteca de Jorge Enrique Robledo</h1>
+    <div class="section news">
+        <h2><span>noticias</span></h2>
+        <!--TODO: Acá se deben mostrar las 6 noticias (categoría) más recientes, debe ir además, antes de las noticias, espacio para el widget de suscribirse-->
+        <div class="row">      
+        <?php $query = new WP_Query (array(
+            'category_name' => 'noticias-home',
+            'orderby' => 'modified',
+            'order' => 'DESC',
+            'posts_per_page' => 6
+        )); ?>
+        <?php $i = 0; ?>
+        <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+            <div class="col-md-4">
+                <a href="<?php the_permalink(); ?>" class="card <?php echo ($i == 0 ? 'bg-primary' : ($i == 2 ? 'card-inverse bg-secondary' : ($i == 4 ? 'card-inverse bg-tertiary' : 'bg-calm'))) ?>">
+                <div class="card-image-header" style="background-image: url(<?php the_post_thumbnail_url( 'medium' ) ?>)">
+                    <div class="card-img-overlay">
+                    <div class="card-content bottom">
+                        <h4 class="card-title"><?php the_title() ?></h4>
+                        <p class="card-text"><small class="text-muted"><?php echo 'Hace ' . human_time_diff( get_the_time('U'), current_time('timestamp') ); ?></small></p>
+                    </div>
+                    </div>
+                </div>
+                <div class="card-text p-3"><?php echo get_the_excerpt(); ?></div>
+                </a>
             </div>
-            </div>
+        <?php $i++; endwhile; endif;?>
         </div>
-        <div class="card-text p-3"><?php echo get_the_excerpt(); ?></div>
-        </a>
     </div>
-    <?php $i++; endwhile; endif;?>
 </div>
 
 <?php

3e8007c118f8e1f2a79ec826edc56fae30c8a74b
M. LeOnardo Mosquera P <mlmosquerap@unal.edu.co>
2017-04-08 15:39:47 -0500

    archivo

--- app/archive.php
+++ app/archive.php
@@ -6,47 +6,50 @@
  *
  * @package robledo-presidente
  */
+get_header();
+?>
 
-get_header(); ?>
+<div class="container-fluid">
+    <div id="primary" class="content-area">
+        
 
-	<div class="container-fluid">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+            <?php if (have_posts()) : ?>
 
-		<?php
-		if ( have_posts() ) : ?>
+                <header class="page-header">
+                    <?php
+                    the_archive_title('<h2 class="colored"><i class="fa fa-newspaper-o" aria-hidden="true"></i> Noticias</h2>', '</h2>');
+                    the_archive_description('<div class="archive-description">', '</div>');
+                    ?>
+                </header><!-- .page-header -->
 
-			<header class="page-header">
-				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+    <div class="row">      
+        <?php $query = new WP_Query (array(
+          'category_name' => 'noticias-home',
+          'orderby' => 'modified',
+          'order' => 'DESC',
+          'posts_per_page' => 6
+        )); ?>
+        <?php $i = 0; ?>
+        <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
+        <div class="col-md-4">
+          <a href="<?php the_permalink(); ?>" class="card">
+            <div class="card-image-header" style="background-image: url(<?php the_post_thumbnail_url( 'medium' ) ?>)">
+              <div class="card-img-overlay">
+                <div class="card-content bottom">
+                  <h4 class="card-title"><?php the_title() ?></h4>
+                </div>
+              </div>
+            </div>
+            <div class="card-text"><small class="text-muted"><?php echo 'Hace ' . human_time_diff( get_the_time('U'), current_time('timestamp') ); ?></small></div>
+            <div class="card-text"><?php echo get_the_excerpt(); ?></div>
+          </a>
+        </div>
+        <?php $i++; endwhile; endif;?>
+      </div>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	</div><!-- .container-fluid -->
+        <!-- #main -->
+    </div><!-- #primary -->
+</div><!-- .container-fluid -->
 
 <?php
 /* get_sidebar(); */

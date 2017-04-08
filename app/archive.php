f2ca0e64f23f23b924d9742c1f02262ac13944e7
M. LeOnardo Mosquera P <mlmosquerap@unal.edu.co>
2017-04-08 16:18:32 -0500

    archive

--- app/archive.php
+++ app/archive.php
@@ -21,46 +21,48 @@ get_header();
                     the_archive_description('<div class="archive-description">', '</div>');
                     ?>
                 </header><!-- .page-header -->
-                <div class="row">
 
+                <div class="container">
+                    <div class="row">
 
-                    <?php
-                    /* Start the Loop */
-                    while (have_posts()) : the_post();
-                        ?>
-                        <div class="col-md-4">
-                            <a href="<?php the_permalink(); ?>" class="card">
-                                <div class="card-image-header" style="background-image: url(<?php the_post_thumbnail_url('medium') ?>)">
-                                    <div class="card-img-overlay">
-                                        <div class="card-content bottom">
-                                            <h4 class="card-title"><?php the_title() ?></h4>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="card-text"><small class="text-muted"><?php echo 'Hace ' . human_time_diff(get_the_time('U'), current_time('timestamp')); ?></small></div>
-                                <div class="card-text"><?php echo get_the_excerpt(); ?></div>
-                            </a>
-                        </div>
-                        /*
-                        * Include the Post-Format-specific template for the content.
-                        * If you want to override this in a child theme, then include a file
-                        * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                        */
-                        /*get_template_part( 'template-parts/content', get_post_format() );*/
 
                         <?php
-                    endwhile;
+                        /* Start the Loop */
+                        while (have_posts()) : the_post();
+                            ?>
+                            <div class="col-md-4">
+                                <a href="<?php the_permalink(); ?>" class="card">
+                                    <div class="card-image-header" style="background-image: url(<?php the_post_thumbnail_url('medium') ?>)">
+                                        <div class="card-img-overlay">
+                                            <div class="card-content bottom">
+                                                <h4 class="card-title"><?php the_title() ?></h4>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-text"><small class="text-muted"><?php echo 'Hace ' . human_time_diff(get_the_time('U'), current_time('timestamp')); ?></small></div>
+                                    <div class="card-text"><?php echo get_the_excerpt(); ?></div>
+                                </a>
+                            </div>
+                            <!-- /*
+                             * Include the Post-Format-specific template for the content.
+                             * If you want to override this in a child theme, then include a file
+                             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                             */
+                             /*get_template_part( 'template-parts/content', get_post_format() );*/-->
 
-                    the_posts_navigation();
+                            <?php
+                        endwhile;
 
-                else :
+                        the_posts_navigation();
 
-                    get_template_part('template-parts/content', 'none');
+                    else :
 
-                endif;
-                ?>
+                        get_template_part('template-parts/content', 'none');
 
+                    endif;
+                    ?>
 
+                </div>
             </div>
 
 

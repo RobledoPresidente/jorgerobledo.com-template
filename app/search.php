9bcc9c7ec5157d5c3e52dea7cb42f10f25d2e28d
M. LeOnardo Mosquera P <mlmosquerap@unal.edu.co>
2017-04-08 17:14:04 -0500

    search.php

--- app/search.php
+++ app/search.php
@@ -9,40 +9,62 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<div class="container">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) : ?>
+            <?php if (have_posts()) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'robledo-presidente' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+                <header class="page-header">
+                   <h1 class="page-title"><?php printf( esc_html__( 'Resultados de búsqueda para: %s', 'robledo-presidente' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+                </header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+                <div class="row">
 
-			endwhile;
 
-			the_posts_navigation();
+                    <?php
+                    /* Start the Loop */
+                    while (have_posts()) : the_post();
+                        ?>
+                        <div class="col-md-4">
+                            <a href="<?php the_permalink(); ?>" class="card">
+                                <div class="card-image-header" style="background-image: url(<?php the_post_thumbnail_url('medium') ?>)">
+                                    <div class="card-img-overlay">
+                                        <div class="card-content bottom">
+                                            <h4 class="card-title"><?php the_title() ?></h4>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-text"><small class="text-muted"><?php echo 'Hace ' . human_time_diff(get_the_time('U'), current_time('timestamp')); ?></small></div>
+                                <div class="card-text"><?php echo get_the_excerpt(); ?></div>
+                            </a>
+                        </div>
+                        <!-- /*
+                         * Include the Post-Format-specific template for the content.
+                         * If you want to override this in a child theme, then include a file
+                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                         */
+                         /*get_template_part( 'template-parts/content', get_post_format() );*/-->
 
-		else :
+                        <?php
+                    endwhile;
 
-			get_template_part( 'template-parts/content', 'none' );
+                    the_posts_navigation();
 
-		endif; ?>
+                else :
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+                    get_template_part('template-parts/content', 'none');
 
+                endif;
+                ?>
+
+            </div>
+
+
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
+</div><!-- .container-fluid -->
 <?php
-get_sidebar();
 get_footer();

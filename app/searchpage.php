e54952b44ea287763e141bf6b73ddc60dda496ef
M. LeOnardo Mosquera P <mlmosquerap@unal.edu.co>
2017-04-08 17:01:10 -0500

    searchpage

---
+++ app/searchpage.php
@@ -0,0 +1,21 @@
+<?php
+/*Template Name: Search Page*/
+/**
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package robledo-presidente
+ */
+ 
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+                    <h1>Prueba</h1>
+		<?php get_search_form(); ?>	
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();

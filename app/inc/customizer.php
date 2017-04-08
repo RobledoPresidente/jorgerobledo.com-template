90cde603bfa9789bdbb79a489c6bcbed26778ad8
rodcruzh <rodrigo.cruz.hz@gmail.com>
2017-04-08 13:41:09 -0500

    Admin panel personalizar hashtag

--- app/inc/customizer.php
+++ app/inc/customizer.php
@@ -14,6 +14,25 @@ function robledo_presidente_customize_register( $wp_customize ) {
 	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
 	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
 	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+	$wp_customize->add_section( 'rp_custom_hashtag_section', array(
+		'title' => __( 'Hashtag' ),
+		'description' => __( 'Agregue hashtag aquí' ),
+		'active_callback' => 'is_front_page',
+		'priority' => 9,
+	) );
+	$wp_customize->add_setting( 'rp_custom_hashtag', array(
+		'type' => 'theme_mod',
+		'default' => '#RobledoPresidente2018',
+	) );
+	$wp_customize->add_control( 'rp_custom_hasthag', array(
+		'type' => 'text',
+		'section' => 'rp_custom_hashtag_section',
+		'label' => __( 'rp_custom_hashtag_section' ),
+		'description' => __( 'Hashtag con el tema destacado de actualidad.' ),
+		'input_attrs' => array(
+			'placeholder' => __( '#RobledoPresidente2018' ),
+		),
+	) );
 }
 add_action( 'customize_register', 'robledo_presidente_customize_register' );
 

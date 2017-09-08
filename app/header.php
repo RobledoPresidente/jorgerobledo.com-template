478493bb7c51abdff0d70f2a979e96cc6ded1329
Sebastián Rojas <sebastian@iridian.co>
2017-09-08 16:06:54 -0500

    optimizaciones

--- app/header.php
+++ app/header.php
@@ -15,7 +15,6 @@
 
 <head>
   <link href="https://fonts.googleapis.com/css?family=Fjalla+One" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Spirax" rel="stylesheet">
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="profile" href="http://gmpg.org/xfn/11"-->
